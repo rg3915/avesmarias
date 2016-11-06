724f8d24ebdc9f73906effb5affbc8d0c391d4ce
rg3915 <rg3915@yahoo.com.br>
2016-11-06 11:15:09 -0200

    Refatorando forms

--- avesmarias/core/forms.py
+++ avesmarias/core/forms.py
@@ -6,5 +6,4 @@ class PersonForm(forms.ModelForm):
 
     class Meta:
         model = Person
-        fields = ['photo', 'first_name', 'last_name',
-                  'email', 'cpf', 'city', 'uf']
+        fields = ['photo']
