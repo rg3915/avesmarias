de76e27ad5ee4b6e2ddb781848884da3a8775c8c
rg3915 <rg3915@yahoo.com.br>
2016-11-06 11:14:53 -0200

    Refatorando o visual recognition

--- visual_recognition.py
+++ visual_recognition.py
@@ -1,9 +1,10 @@
 import json
 from os.path import join, dirname
-from os import environ
 from watson_developer_cloud import VisualRecognitionV3
 
-visual_recognition = VisualRecognitionV3('2016-05-20', api_key='')
+visual_recognition = VisualRecognitionV3(
+    '2016-11-06', api_key='')
 
 with open(join(dirname(__file__), 'barack.jpg'), 'rb') as image_file:
-    print(json.dumps(visual_recognition.detect_faces(images_file=image_file), indent=2))
+    print(json.dumps(visual_recognition.detect_faces(
+        images_file=image_file), indent=2))
