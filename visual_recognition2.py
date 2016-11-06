70efab35b6cda5b3c7ff8b9c4b66cd4dbe29a33f
rg3915 <rg3915@yahoo.com.br>
2016-11-06 12:06:16 -0200

    Refatorando visual_recognition2

--- visual_recognition2.py
+++ visual_recognition2.py
@@ -1,12 +1,10 @@
-import json
-from os.path import join, dirname
+from decouple import config
 from watson_developer_cloud import VisualRecognitionV3
 
-# test_url = 'https://www.ibm.com/ibm/ginni/images/ginni_bio_780x981_v4_03162016.jpg'
-# test_url = 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/7e/Thomas_J_Watson_Sr.jpg/800px-Thomas_J_Watson_Sr.jpg'
 test_url = 'http://tudosobrecachorros.com.br/wp-content/uploads/cachorro-independente.jpg'
 
-visual_recognition = VisualRecognitionV3('2016-11-06', api_key='aadd9bb59c10ec90e0e6ee622a855e70431a0054')
+visual_recognition = VisualRecognitionV3(
+    '2016-11-06', api_key=config('API_KEY'))
 
 analise = visual_recognition.classify(images_url=test_url)
 if analise['images'][0]['classifiers'][0]['classes'][0]['class'] == 'person':
