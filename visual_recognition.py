1173217f4e36ea88d9d174c24f987cefc2e9aed5
rg3915 <rg3915@yahoo.com.br>
2016-11-06 11:17:38 -0200

    Refatorando visual recognitions

--- visual_recognition.py
+++ visual_recognition.py
@@ -1,9 +1,10 @@
 import json
+from decouple import config
 from os.path import join, dirname
 from watson_developer_cloud import VisualRecognitionV3
 
 visual_recognition = VisualRecognitionV3(
-    '2016-11-06', api_key='')
+    '2016-11-06', api_key=config('API_KEY'))
 
 with open(join(dirname(__file__), 'barack.jpg'), 'rb') as image_file:
     print(json.dumps(visual_recognition.detect_faces(
